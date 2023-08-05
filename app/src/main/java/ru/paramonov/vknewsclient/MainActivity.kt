88cb0cfe101624a533a35ee244a749c7cc506f84
nikita <paramonov20@bk.ru>
2023-08-05 22:47:07 +0300

    Подготовил экран для добавления viewmodel

--- app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
@@ -3,6 +3,14 @@ package ru.paramonov.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import ru.paramonov.vknewsclient.ui.InstagramProfileCard
 import ru.paramonov.vknewsclient.ui.MainScreen
 import ru.paramonov.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -11,7 +19,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
-                MainScreen()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
+                ) {
+                    InstagramProfileCard()
+                }
             }
         }
     }
---
+++ app/src/main/java/ru/paramonov/vknewsclient/ui/InstagramProfileCard.kt
@@ -0,0 +1,143 @@
+package ru.paramonov.vknewsclient.ui
+
+import android.widget.Space
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.paramonov.vknewsclient.R
+import ru.paramonov.vknewsclient.ui.theme.VkNewsClientTheme
+
+@Composable
+fun InstagramProfileCard() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 8.dp,
+            topEnd = 8.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        border = BorderStroke(
+            width = 2.dp, color = MaterialTheme.colorScheme.onPrimary
+        ),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.insta),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(56.dp)
+                        .clip(shape = CircleShape)
+                        .background(color = Color.White)
+                        .padding(8.dp),
+                    contentScale = ContentScale.FillBounds
+                )
+                UserStatistics(value = "7,950", title = "Posts")
+                UserStatistics(value = "500M", title = "Followers")
+                UserStatistics(value = "24", title = "Following")
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Instagram",
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "#LerningJetpackCompose")
+            Text(text = "@Nikitosik_73")
+
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    contentColor = MaterialTheme.colorScheme.primary
+                )
+            ) {
+                Text(
+                    text = "Follow"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun UserStatistics(
+    value: String,
+    title: String
+) {
+    Column (
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.height(80.dp)
+    ) {
+        Text(
+            text = value,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive
+        )
+        Text(
+            text = title,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LightPreviewCard() {
+    VkNewsClientTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun DarkPreviewCard() {
+    VkNewsClientTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
+    }
+}
