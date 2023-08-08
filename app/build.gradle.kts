34a29064f5df6288dca0b288137f24942eb6370c
nikita <paramonov20@bk.ru>
2023-08-08 21:04:42 +0300

    Изучил функцию SwipeToDismiss, и реализовал удаления элемента списка по свайпу

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.paramonov.vknewsclient"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.paramonov.vknewsclient"
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha03")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
@@ -7,16 +7,26 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.paramonov.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +41,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun TestLazyColumn(viewModel: MainViewModel) {
     VkNewsClientTheme {
@@ -40,12 +51,40 @@ private fun TestLazyColumn(viewModel: MainViewModel) {
                 .background(MaterialTheme.colorScheme.background)
         ) {
             val models = viewModel.models.observeAsState(emptyList())
-            LazyVerticalGrid(columns = GridCells.Fixed(count = 2)) {
-                items(models.value) { model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowButtonClickListener = { instagramModel ->
-                            viewModel.changeFollowingStatus(instagramModel)
+            LazyColumn {
+                items(models.value, key = { it.id }) { model ->
+                    val dismissState = rememberDismissState()
+
+                    if (dismissState.isDismissed(direction = DismissDirection.EndToStart)) {
+                        viewModel.deleteItem(model)
+                    }
+
+                    SwipeToDismiss(
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        background = {
+                            Box(
+                                contentAlignment = Alignment.CenterEnd,
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxSize()
+                                    .background(Color.Red.copy(alpha = 0.5f))
+                            ) {
+                                Text(
+                                    text = "Delete Item",
+                                    color = Color.White,
+                                    fontSize = 22.sp,
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        },
+                        dismissContent = {
+                            InstagramProfileCard(
+                                model = model,
+                                onFollowButtonClickListener = { instagramModel ->
+                                    viewModel.changeFollowingStatus(instagramModel)
+                                }
+                            )
                         }
                     )
                 }
