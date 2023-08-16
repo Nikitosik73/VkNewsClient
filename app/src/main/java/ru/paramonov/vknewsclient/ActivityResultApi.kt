5ff5a36a14aa1dc5f0f369b432a94851e0912656
nikita <paramonov20@bk.ru>
2023-08-17 01:28:40 +0300

    Узнал, как пользоваться activityResultApi в jectpack compose, также попрактиковался с библиотекой coil для установки изображений

---
+++ app/src/main/java/ru/paramonov/vknewsclient/ActivityResultApi.kt
@@ -0,0 +1,62 @@
+package ru.paramonov.vknewsclient
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun ActivityResultApiTest() {
+
+    var imageUri by remember { mutableStateOf(Uri.EMPTY) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            uri?.let { imageUri = uri }
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        AsyncImage(
+            model = imageUri,
+            contentDescription = null,
+            modifier = Modifier.weight(1f)
+        )
+        Button(
+            onClick = {
+                launcher.launch("image/*")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Blue
+            )
+        ) {
+            Text(text = "Get Image")
+        }
+    }
+}
