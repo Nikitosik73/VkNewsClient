3ef492303693ebe43dd70daf7c0fa670351f5204
nikita <paramonov20@bk.ru>
2023-08-17 22:08:27 +0300

    Узнал, что такое sideEffect и launchedEffect, и в чём их отличия

--- app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
+++ app/src/main/java/ru/paramonov/vknewsclient/MainActivity.kt
@@ -5,6 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKAuthenticationResult
 import com.vk.api.sdk.auth.VKScope
@@ -16,6 +25,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
+                val someState = remember { mutableStateOf(true) }
+                Log.d("MainActivity", "Recomposition: ${someState.value}")
                 val authLauncher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract(),
                     onResult = { result ->
@@ -29,8 +40,23 @@ class MainActivity : ComponentActivity() {
                         }
                     }
                 )
-                authLauncher.launch(listOf(VKScope.WALL))
-                ActivityResultApiTest()
+                LaunchedEffect(key1 = Unit) {
+                    Log.d("MainActivity", "LaunchedEffect")
+                }
+                SideEffect {
+                    Log.d("MainActivity", "SideEffect")
+                }
+                Button(
+                    onClick = {
+                        someState.value = !someState.value
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(text = "change state")
+                }
             }
         }
     }
---
+++ app/src/main/java/ru/paramonov/vknewsclient/SideEffectTest.kt
@@ -0,0 +1,37 @@
+package ru.paramonov.vknewsclient
+
+import android.os.Handler
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SideEffectTest(number: MyNumber) {
+    Column {
+        LazyColumn {
+            repeat(5) {
+                item {
+                    Text(text = "${number.value}")
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Handler().postDelayed({
+            number.value = 5
+        }, 3000)
+        LazyColumn {
+            repeat(5) {
+                item {
+                    Text(text = "${number.value}")
+                }
+            }
+        }
+    }
+}
+
+data class MyNumber(var value: Int)
