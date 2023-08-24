4f3a207a76856bad23a8824994ba87e8ad47301f
nikita <paramonov20@bk.ru>
2023-08-24 08:29:07 +0300

    Попрактиковался с coroutine flow, сделал загрузку данных с помошью flow, также загрузку следующих данных при скроле, удаление элемента из списка и установку лайков

--- app/src/main/java/ru/paramonov/vknewsclient/presentation/screens/newsfeed/NewsFeedViewModel.kt
+++ app/src/main/java/ru/paramonov/vknewsclient/presentation/screens/newsfeed/NewsFeedViewModel.kt
@@ -5,45 +5,51 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import ru.paramonov.vknewsclient.data.repository.NewsFeedRepository
 import ru.paramonov.vknewsclient.domain.FeedPost
-import ru.paramonov.vknewsclient.domain.StatisticItem
+import ru.paramonov.vknewsclient.extensions.mergeWith
 
 class NewsFeedViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val initialState = NewsFeedScreenState.Initial
-
-    private val _screenState = MutableLiveData<NewsFeedScreenState>(initialState)
-    val screenState: LiveData<NewsFeedScreenState> = _screenState
-
     private val repository = NewsFeedRepository(application)
+    private val allNewsFeedFlow = repository.allNewsFeed
 
-    init {
-        _screenState.value = NewsFeedScreenState.Loading
-        loadAllNewsFeed()
+    private val nextLoadDataEvents = MutableSharedFlow<Unit>()
+    private val nextLoadDataFlow = flow {
+        nextLoadDataEvents.collect {
+            emit(
+                NewsFeedScreenState.Posts(
+                    posts = allNewsFeedFlow.value,
+                    nextDataLoading = true
+                )
+            )
+        }
     }
 
-    private fun loadAllNewsFeed() = viewModelScope.launch {
-        val newsFeeds = repository.loadAllNewsFeed()
-        _screenState.value = NewsFeedScreenState.Posts(posts = newsFeeds)
-    }
+    val screenState: Flow<NewsFeedScreenState> = allNewsFeedFlow
+        .filter { it.isNotEmpty() }
+        .map { NewsFeedScreenState.Posts(posts = it) as NewsFeedScreenState }
+        .onStart { emit(NewsFeedScreenState.Loading) }
+        .mergeWith(another = nextLoadDataFlow)
 
-    fun loadNextDataNewsFeed() {
-        _screenState.value = NewsFeedScreenState.Posts(
-            posts = repository.feedPost,
-            nextDataLoading = true
-        )
-        loadAllNewsFeed()
+    fun loadNextDataNewsFeed() = viewModelScope.launch {
+        nextLoadDataEvents.emit(Unit)
+        repository.loadNextData()
     }
 
     fun changeLikeStatus(feedPost: FeedPost) = viewModelScope.launch {
         repository.changeLikeStatus(feedPost)
-        _screenState.value = NewsFeedScreenState.Posts(posts = repository.feedPost)
     }
 
     fun deletePost(feedPost: FeedPost) = viewModelScope.launch {
         repository.deletePost(feedPost = feedPost)
-        _screenState.value = NewsFeedScreenState.Posts(posts = repository.feedPost)
     }
 }
